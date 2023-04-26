468502befd6362b5ab9093ab42b9d066611f34e8
maximoortelli <118080546+maximoortelli@users.noreply.github.com>
2023-04-26 19:43:55 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+function mostrar_menu(){
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+}/*Esto me permite que me muestre el menu bar y se despliegue*/ 
+
+window.onscroll = function (){
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var elemento1 = document.getElementById("icon_heart");
+    var elemento2 = document.getElementById("icon_fire");
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.1 + "px"; 
+} /*Este bloque de codigo hace que la pagina sea responsive*/
